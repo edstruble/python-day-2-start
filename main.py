da5bf4a627a4606f181e57b00ddcb75df905b98f
edstruble <edstruble@gmail.com>
2024-01-10 03:04:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Data Types
+
+#String
+print("Hello"[0])
+print("Hello"[4])
+#numbers in parenthesis are strings "12345" is NOT a number
+#this will concatenate these two strings as 12345678
+print("1234" + "5678")
+
+#integers write number without anything
+#this will calculate 579
+print(123 + 456)
+#you can put underscores to make large numbers easier to read
+print(123_124_234)
+
+#float
+3.14159
+
+#boolean
+True
+False
